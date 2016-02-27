713df28fa9eca10e988415c4e8245cb889d7d5a1
Alexandra Jones <alexandra.a.jones@gmail.com>
2016-02-27 12:45:12 +0000

    added fs

--- server.js
+++ server.js
@@ -1,12 +1,52 @@
 var http = require('http');
-
-var message = 'I am so happy to be part of the Node Girls workshop!';
+var fs = require('fs');
 
 function handler (request, response) {
+
+  if (endpoint === "/") {
+    response.writeHead(200, {"Content-Type": "text/html"});
+
+    fs.readFile(__dirname + '/public/index.html', function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      response.end(file);
+    });
+  }
+}
+
+var http = require('http');
+
+var message = 'I am so happy to be part of the Node Girls workshop!';
+var message1 = 'Node is so easy!';
+var message2 = 'Girls rule boys drool.';
+
+function handler (request, response) {
+
+  var endpoint = request.url;
+  console.log(endpoint);
+
+  var method = request.method;
+  console.log(method);
+
+if (endpoint === '/node'){
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(message1); //response body
+  response.end(); // finish response
+  }
+
+else if (endpoint ==='/girls') {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(message2); //response body
+  response.end(); // finish response
+  }
+else if (endpoint ==='/') {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write(message); //response body
   response.end(); // finish response
-
+    }
 }
 
 
